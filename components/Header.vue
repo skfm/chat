aa930402dd55f3481346fa73f564c5c6e6ec9667
seiya <skkasans@gmail.com>
2020-08-11 23:50:17 +0900

    ログインページの作成

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="ixed top-0 bg-lightblue w-full max-w-sm h-16 py-4 z-10">
+    <div class="relative flex justify-center w-full">
+      <h1 class="text-2xl font-semibold text-white">tech-chat</h1>
+    </div>
+  </header>
+</template>
