89b842079ba034a4142a760cd21809ee2b6e060f
seiya <skkasans@gmail.com>
2020-08-14 09:00:51 +0900

    チャットルーム登録機能を実装

--- test/firestore.spec.js
+++ test/firestore.spec.js
@@ -11,24 +11,20 @@ describe(testName, () => {
     })
   })
 
-  // テスト終了後にデータを削除します
   afterEach(async () => {
     await firebase.clearFirestoreData({ projectId: testName })
   })
 
-  // テスト終了後にアプリを削除します
   afterAll(async () => {
     await Promise.all(firebase.apps().map((app) => app.delete()))
   })
 
-  // 認証済のFirestore
   function authDB(auth) {
     return firebase
       .initializeTestApp({ projectId: testName, auth: auth })
       .firestore()
   }
 
-  // 未認証のFirestore
   function noAuthDB() {
     return firebase
       .initializeTestApp({ projectId: testName, auth: null })
@@ -88,6 +84,88 @@ describe(testName, () => {
             name: 'testUser',
             iconImageUrl: 'https://exampla.com',
           })
+        )
+      })
+    })
+  })
+
+  describe('rooms collection', () => {
+    describe('read', () => {
+      test('未ログインの場合、Roomデータ取得に失敗するかどうか', async () => {
+        const db = noAuthDB()
+        await firebase.assertFails(
+          db
+            .collection('rooms')
+            .doc('tech-room')
+            .get()
+        )
+      })
+
+      test('ログイン済の場合、Roomデータを取得できるかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertSucceeds(
+          db
+            .collection('rooms')
+            .doc('tech-room')
+            .get()
+        )
+      })
+    })
+
+    describe('create', () => {
+      test('Roomのデータを作成できるかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertSucceeds(
+          db
+            .collection('rooms')
+            .doc('tech-room')
+            .set({
+              name: 'なんとかについて話す部屋です',
+              topImageUrl: 'https://exampla.com',
+              createdAt: new Date()
+            })
+        )
+      })
+
+      test('部屋名が未入力の場合、作成に失敗するかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertFails(
+          db
+            .collection('rooms')
+            .doc('tech-room')
+            .set({
+              name: '',
+              topImageUrl: 'https://exampla.com',
+              createdAt: new Date()
+            })
+        )
+      })
+
+      test('TOP画像が未入力の場合、作成に失敗するかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertFails(
+          db
+            .collection('rooms')
+            .doc('tech-room')
+            .set({
+              name: 'testUser',
+              topImageUrl: '',
+              createdAt: new Date()
+            })
+        )
+      })
+
+      test('作成日が未入力の場合、作成に失敗するかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertFails(
+          db
+            .collection('rooms')
+            .doc('tech-room')
+            .set({
+              name: 'testUser',
+              topImageUrl: 'https://exampla.com',
+              createdAt: ''
+            })
         )
       })
     })
